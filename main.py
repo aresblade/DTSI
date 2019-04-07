04f08436a63621a766533a13d153741121c04dbc
Kamil Wójcik <kamil.wojcik@cognifide.com>
2019-04-07 22:07:30 +0200

    Creating read_request method

--- main.py
+++ main.py
@@ -2,13 +2,48 @@ import datetime
 import time
 import sqlite3
 import unidecode
+import itertools
 
+fuzzy_values = ["low", "medium", "high", "critical"]
+fuzzy_var_inputs = ["temperature", "voltage", "humidity"]
 
-database = sqlite3.connect('/home/jamedia/jamedia.db')
-cursor = database.cursor()
-sql = "SELECT VOLTAGE FROM DANE WHERE STATION_ID = "+ stationid +" AND ID IN (SELECT max(ID) FROM DANE GROUP BY STATION_ID) LIMIT 3;"
-cursor.execute(sql)
-for voltage in cursor:
-	#usefull loop
-sql = "SELECT ID,STATION_ID,VOLTAGE,CURRENT,TEMPERATURE,HUMIDITY,datetime(DATE,'localtime') as DATE FROM DANE WHERE STATION_ID = "+str(station) + " ORDER BY " + orderby;
-database.close()
+#Generate all combinations of words 
+def set_all_combinations():
+    all_combinations = []
+    tuples_list = list(itertools.product(fuzzy_values, fuzzy_var_inputs))
+
+    for combination in tuples_list:
+        all_combinations.append(' '.join(combination))
+
+    return all_combinations
+
+def read_request(string):
+    request_values_list = []
+    words_combinations = set_all_combinations()
+
+    for combination in words_combinations:
+        if combination in string:
+            request_values_list.append(combination)
+
+    # print(request_values_list)
+    return request_values_list
+
+def main():
+    print("Welcome in fuzzy SQL")
+
+    database = sqlite3.connect('./jamedia.db')
+    cursor = database.cursor()
+    sql = "SELECT VOLTAGE FROM DANE"
+    cursor.execute(sql)
+
+    request = input('Write request: ')
+    read_request(request)
+
+    database.close()
+
+if __name__ == '__main__':
+    main()
+
+# for voltage in cursor:
+#usefull loop
+# sql = "SELECT ID,STATION_ID,VOLTAGE,CURRENT,TEMPERATURE,HUMIDITY,datetime(DATE,'localtime') as DATE FROM DANE WHERE STATION_ID = "+str(station) + " ORDER BY " + orderby;
