fe53b4d54127b9a43b4236cfdd105c48fd443a5e
Grzegorz Stępień <aresblade18@gmail.com>
2019-04-12 21:57:36 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ from prettytable import PrettyTable
 import skfuzzy as fuzz
 import numpy as np
 import re, csv, sys
+import os
+import glob
+import csv
+from xlsxwriter.workbook import Workbook
+
 from skfuzzy import control as ctrl
 
 class FUZZY_OBJECT:
@@ -102,6 +107,16 @@ def main():
         w = csv.writer(x)
         w.writerows(pretty_table_to_tuples(str(table)))
     database.close()
+    
+    for csvfile in glob.glob(os.path.join('.', '*.csv')):
+        workbook = Workbook(csvfile[:-4] + '.xlsx')
+        worksheet = workbook.add_worksheet()
+        with open(csvfile, 'rt', encoding='utf8') as f:
+            reader = csv.reader(f)
+            for r, row in enumerate(reader):
+                for c, col in enumerate(row):
+                    worksheet.write(r, c, col)
+        workbook.close()
 
 if __name__ == '__main__':
     main()
